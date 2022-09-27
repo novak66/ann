7ae5d94f27139afc7e0955d1e3db7ca30df294c4
GeeksforGeeks <lucasnovak66@gmail.com>
2022-09-26 23:41:51 -0300

    new commit

--- interpolacao/src/main.java
+++ interpolacao/src/main.java
@@ -1,28 +1,60 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class main {
 
     public static void main(String args[]) {
         List<Double> x = new ArrayList<>();
-        x.add(1.142);
-        x.add(2.62);
-        x.add(5.066);
-
+        x.add(0.456);
+        x.add(0.974);
+        x.add(1.557);
+        x.add(2.16);
+        x.add(2.662);
+        x.add(3.146);
+        x.add(3.571);
+        x.add(4.198);
+        x.add(5.164);
+        x.add(5.719);
+        x.add(6.312);
+        x.add(6.865);
         List<Double> y = new ArrayList<>();
 
-        y.add(4.866);
-        y.add(4.269);
-        y.add(2.206);
-
+        y.add(4.433);
+        y.add(4.796);
+        y.add(4.919);
+        y.add(4.676);
+        y.add(4.225);
+        y.add(3.666);
+        y.add(3.159);
+        y.add(2.542);
+        y.add(2.211);
+        y.add(2.375);
+        y.add( 2.701);
+        y.add( 2.979);
         vanderonde(x, y);
     }
 
-    public static void gauss(List<List<Double>> mat ) {
-
-
-        System.out.println(mat);
+    public static double[] solveSystem(List<List<Double>> A, List<Double> b) {
+        double[] x = initialize(new double[A.get(0).size()]);
+        for (int k = 0; k < 100000000; k++) {
+            for (int i = 0; i < A.size(); i++) {
+                double x0 = 0;
+                for (int j = 0; j < A.size(); j++)
+                    if (i != j)
+                        x0 += A.get(i).get(j) * x[j];
+                x[i] = (b.get(i) - x0)/A.get(i).get(i);
+            }
+        }
+        return x;
     }
+
+    private static double[] initialize(double[] output) {
+        for (int i = 0; i < output.length; i++)
+            output[i] = 0;
+        return output;
+    }
+
 
     public static void multiplicar(List<List<Double>> mat, List<Double> B) {
         int i = 0;
@@ -58,23 +90,23 @@ public class main {
                 row.set(k, Math.pow(xi, k));
             }
 
-            if (A.size() == 0) {
-                List<Double> row2 = new ArrayList<>();
-
-                for (Double item : row) {
-                    row2.add(item);
-                }
-
-                A.add(row2);
-            } else {
-
-                for (int j = 0; j < row.size(); j++) {
-                    A.get(i).add(row.get(j));
-                }
-                i++;
-            }
+            preencherMatr(A, row);
         }
 
-        System.out.println(A);
+      double[] coesf =   solveSystem(A, B);
+        Locale.setDefault(Locale.US);
+        for(int k=0; k< coesf.length; k++) {
+            System.out.printf("%.16f, ", coesf[k]);
+        }
+    }
+
+    public static void preencherMatr(List<List<Double>> A, List<Double> row) {
+        List<Double> newRow = new ArrayList<>();
+
+        for(Double item: row) {
+            newRow.add(item);
+        }
+
+        A.add(newRow);
     }
 }
